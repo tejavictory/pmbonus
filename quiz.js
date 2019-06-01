b0d87b618db688b1ddb25e337827a99a2a678699
Kancharla, Sai Krishna Teja <42943403+tejavictory@users.noreply.github.com>
2019-06-01 18:32:12 -0500

    files uploaded

---
+++ quiz.js
@@ -0,0 +1,144 @@
+// https://www.simplilearn.com/pmp-sample-questions-and-answers-article
+(function() {
+    var questions = [{
+      question: "A project has a 60% chance of a $100,000 profit and a 40 percent of a US $100,000 loss. The Expected Monetary Value for the project is:",
+      choices: ["$100K profit","$60K loss","$20K profit","$40K loss"],
+      correctAnswer: 2
+    },
+    {
+      question: "Which of the following statement is true about a Program?",
+      choices: ["A Program is a group of related projects","A Program is group of unrelated projects","A Program is a part of a big project","None of above"],
+      correctAnswer: 0
+    }];
+    
+    var questionCounter = 0; 
+    var selections = []; 
+    var quiz = $('#quiz'); 
+    
+    displayNext();
+    
+    $('#next').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {        
+        return false;
+      }
+      choose();
+      
+      if (isNaN(selections[questionCounter])) {
+        alert('Please make a selection!');
+      } else {
+        questionCounter++;
+        displayNext();
+      }
+    });
+    
+    $('#prev').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      choose();
+      questionCounter--;
+      displayNext();
+    });
+    
+    $('#start').on('click', function (e) {
+      e.preventDefault();
+      
+      if(quiz.is(':animated')) {
+        return false;
+      }
+      questionCounter = 0;
+      selections = [];
+      displayNext();
+      $('#start').hide();
+    });
+    
+    $('.button').on('mouseenter', function () {
+      $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function () {
+      $(this).removeClass('active');
+    });
+    
+    function createQuestionElement(index) {
+      var qElement = $('<div>', {
+        id: 'question'
+      });
+      
+      var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+      
+      var question = $('<p>').append(questions[index].question);
+      qElement.append(question);
+      
+      var radioButtons = createRadios(index);
+      qElement.append(radioButtons);
+      
+      return qElement;
+    }
+    
+    function createRadios(index) {
+      var radioList = $('<ul>');
+      var item;
+      var input = '';
+      for (var i = 0; i < questions[index].choices.length; i++) {
+        item = $('<li>');
+        input = '<input type="radio" name="answer" value=' + i + ' />';
+        input += questions[index].choices[i];
+        item.append(input);
+        radioList.append(item);
+      }
+      return radioList;
+    }
+
+    function choose() {
+      selections[questionCounter] = +$('input[name="answer"]:checked').val();
+    }
+    
+    function displayNext() {
+      quiz.fadeOut(function() {
+        $('#question').remove();
+        
+        if(questionCounter < questions.length){
+          var nextQuestion = createQuestionElement(questionCounter);
+          quiz.append(nextQuestion).fadeIn();
+          if (!(isNaN(selections[questionCounter]))) {
+            $('input[value='+selections[questionCounter]+']').prop('checked', true);
+          }
+          
+          // Controls display of 'prev' button
+          if(questionCounter === 1){
+            $('#prev').show();
+          } else if(questionCounter === 0){
+            
+            $('#prev').hide();
+            $('#next').show();
+          }
+        }else {
+          var scoreElem = displayScore();
+          quiz.append(scoreElem).fadeIn();
+          $('#next').hide();
+          $('#prev').hide();
+          $('#start').show();
+        }
+      });
+    }
+    
+    function displayScore() {
+      var score = $('<p>',{id: 'question'});
+      
+      var numCorrect = 0;
+      for (var i = 0; i < selections.length; i++) {
+        if (selections[i] === questions[i].correctAnswer) {
+          numCorrect++;
+        }
+      }
+      
+      score.append('You got ' + numCorrect + ' questions out of ' +
+                   questions.length + ' right!!!');
+      return score;
+    }
+  })();
